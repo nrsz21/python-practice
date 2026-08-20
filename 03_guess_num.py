be1759c95c407a8998382551d0b3f09cd4335312
nrsz21 <nrsz21@users.noreply.github.com>
2026-08-20 18:49:17 +0800

    Day3：九九乘法表函数优化，修正无返回值调用

--- 03_guess_num.py
+++ 03_guess_num.py
@@ -19,29 +19,29 @@ import random
 #     else:
 #         print('小了')
 #将乘法表写入函数
-# def nn(num):
-#     for i in range(1,num):
-#         for j in range(1,i+1):
-#             print('{} * {} = {}'.format(j,i,i*j),end='\t')
-#         print('')
-# if __name__ == '__main__':
-#     print(nn(11))
-def guess_number(mini,max):
-    num = random.randint(mini, max)
-    while True:
-        a = input('请输入你猜测的数字：')
-        if not a.isdigit():#判断是不是数字
-            print('请输入数字')
-            continue
-        if int(a) > num:
-            print('大了')
-        elif int(a)==num:
-            print('猜对了')
-            break
-        else:
-            print('小了')
+def nn(num):
+    for i in range(1,num+1):
+        for j in range(1,i+1):
+            print('{} * {} = {}'.format(j,i,i*j),end='\t')
+        print('')
 if __name__ == '__main__':
-    guess_number(1,50)
+    nn(9)
+# def guess_number(mini,max):
+#     num = random.randint(mini, max)
+#     while True:
+#         a = input('请输入你猜测的数字：')
+#         if not a.isdigit():#判断是不是数字
+#             print('请输入数字')
+#             continue
+#         if int(a) > num:
+#             print('大了')
+#         elif int(a)==num:
+#             print('猜对了')
+#             break
+#         else:
+#             print('小了')
+# if __name__ == '__main__':
+#     guess_number(1,50)
 
 
 
